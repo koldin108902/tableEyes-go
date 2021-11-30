7d10cc96d817e928719d5e939a824351eb72027a
koldin108902 <alex108902@naver.com>
2021-12-01 01:38:03 +0900

    db struct 파일로 분활

---
+++ struct/row.go
@@ -0,0 +1,13 @@
+package _struct
+
+type Row struct {
+	Id           int
+	OwnerId      int
+	Name         string
+	Description  string
+	Category     string
+	WaitingState bool
+	Detail       string
+	IsOpen       bool
+	NowPersonnel int
+}
